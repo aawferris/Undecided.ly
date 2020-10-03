7ec4660984c43e14f2e565ba026fb97ed991ee85
Andrew <aawferris@gmail.com>
2020-10-03 17:47:41 -0400

    Updates

---
+++ precache-manifest.31563c6e9b966db47cc754f834ecdcdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176f2312b0f64a0d318c0f96600f5970",
+    "url": "/Undecided.ly/index.html"
+  },
+  {
+    "revision": "c92d32edb008835ef4d5",
+    "url": "/Undecided.ly/static/css/main.73f14273.chunk.css"
+  },
+  {
+    "revision": "0fb4d8f4b93b7bea57fd",
+    "url": "/Undecided.ly/static/js/2.9742db41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Undecided.ly/static/js/2.9742db41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92d32edb008835ef4d5",
+    "url": "/Undecided.ly/static/js/main.7010dbc5.chunk.js"
+  },
+  {
+    "revision": "54ef4f241cbdfd159f10",
+    "url": "/Undecided.ly/static/js/runtime-main.0a72a6d3.js"
+  }
+]);
